7438773f5e11f5cc7362e0b010c2b883ae3e115f
TSchultz225 <tschultz225@gmail.com>
2025-12-07 15:05:49 -0500

    book characters function first pass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_book_words
+from stats import get_book_characters
 
 def get_book_text(filepath):
     
@@ -9,10 +10,10 @@ def get_book_text(filepath):
 
     return file_contents
 
-
-
 def main():
-    num_words = get_book_words(get_book_text("./books/frankenstein.txt"))
+    book_contents = get_book_text("./books/frankenstein.txt")
+    num_words = get_book_words(book_contents)
     print(f"Found {num_words} total words")
+    print(get_book_characters(book_contents))
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,16 @@ def get_book_words(book_contents):
     number_of_words = len(book_contents.split())
 
     return number_of_words
+
+def get_book_characters(book_contents):
+    lower_contents = book_contents.lower()
+    char_dic = {}
+
+    for char in lower_contents:
+
+        if char_dic.get(char) == None:
+            char_dic[char]=0
+
+        char_dic[char] += 1
+    
+    return char_dic
