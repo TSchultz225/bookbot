0fca7deb6f6b6649c95cb89283a4bbb59afb2ff4
TSchultz225 <tschultz225@gmail.com>
2025-12-07 14:56:18 -0500

    update split

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_book_text(filepath):
 def get_book_words(book_contents):
     number_of_words=0
 
-    number_of_words = len(book_contents.split(' '))
+    number_of_words = len(book_contents.split())
 
     return number_of_words
 
