74770b2145f689f4ba887c469b12703f310723bc
TSchultz225 <tschultz225@gmail.com>
2025-12-07 14:51:25 -0500

    add print statement

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def get_book_text(filepath):
+    
     file_contents=""
+
     with open(filepath) as f:
         file_contents = f.read()
 
     return file_contents
 
 def main():
-    get_book_text("./books/frankenstein.txt")
+    print(get_book_text("./books/frankenstein.txt"))
 
 main()
